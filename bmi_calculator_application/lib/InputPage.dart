ae11a5f7c391eb6b4d93bd29fcd3aa284c2c8354
Rahul Khatav <rahulskhatav@gmail.com>
2022-10-15 22:58:48 +0530

    Slider for height added

--- bmi_calculator_application/lib/InputPage.dart
+++ bmi_calculator_application/lib/InputPage.dart
@@ -91,6 +91,29 @@ class _InputPageState extends State<InputPage> {
                       )
                     ],
                   ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Color(0xFFEB1555),
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape: RoundSliderThumbShape(
+                        enabledThumbRadius: 15.0,
+                      ),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double value) {
+                        setState(() {
+                          height = value.round();
+                        });
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
